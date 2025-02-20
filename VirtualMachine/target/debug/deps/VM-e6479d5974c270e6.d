0455810e933468ae5bfa381e0ccacfa6cdc7774e
mlazoy <mlazoy@icloud.com>
2025-02-20 23:57:39 +0200

    ran some examples

--- VirtualMachine/target/debug/deps/VM-e6479d5974c270e6.d
+++ VirtualMachine/target/debug/deps/VM-e6479d5974c270e6.d
@@ -1,6 +1,6 @@
-/Users/marialazou/Desktop/semester_9/progl2/homework10/target/debug/deps/VM-e6479d5974c270e6: src/main.rs src/bytecode.rs src/vm.rs src/heap.rs
+/Users/marialazou/miniML/VirtualMachine/target/debug/deps/VM-e6479d5974c270e6: src/main.rs src/bytecode.rs src/vm.rs src/heap.rs
 
-/Users/marialazou/Desktop/semester_9/progl2/homework10/target/debug/deps/VM-e6479d5974c270e6.d: src/main.rs src/bytecode.rs src/vm.rs src/heap.rs
+/Users/marialazou/miniML/VirtualMachine/target/debug/deps/VM-e6479d5974c270e6.d: src/main.rs src/bytecode.rs src/vm.rs src/heap.rs
 
 src/main.rs:
 src/bytecode.rs:
--- VirtualMachine/target/release/deps/VM-bc5f2084e1dfd13e.d
+++ VirtualMachine/target/release/deps/VM-bc5f2084e1dfd13e.d
@@ -1,6 +1,6 @@
-/Users/marialazou/Desktop/semester_9/progl2/homework10/target/release/deps/VM-bc5f2084e1dfd13e: src/main.rs src/bytecode.rs src/vm.rs src/heap.rs
+/Users/marialazou/miniML/VirtualMachine/target/release/deps/VM-bc5f2084e1dfd13e: src/main.rs src/bytecode.rs src/vm.rs src/heap.rs
 
-/Users/marialazou/Desktop/semester_9/progl2/homework10/target/release/deps/VM-bc5f2084e1dfd13e.d: src/main.rs src/bytecode.rs src/vm.rs src/heap.rs
+/Users/marialazou/miniML/VirtualMachine/target/release/deps/VM-bc5f2084e1dfd13e.d: src/main.rs src/bytecode.rs src/vm.rs src/heap.rs
 
 src/main.rs:
 src/bytecode.rs:
